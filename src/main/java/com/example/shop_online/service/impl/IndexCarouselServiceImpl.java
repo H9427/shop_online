d236dda26bce8ea335699bc140a439e53184272a
hbin <11323063@qq.com>
2023-11-09 20:33:36 +0800

    后端项目搭建

---
+++ src/main/java/com/example/shop_online/service/impl/IndexCarouselServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.shop_online.service.impl;
+
+import com.example.shop_online.entity.IndexCarousel;
+import com.example.shop_online.mapper.IndexCarouselMapper;
+import com.example.shop_online.service.IndexCarouselService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author whb
+ * @since 2023-11-09
+ */
+@Service
+public class IndexCarouselServiceImpl extends ServiceImpl<IndexCarouselMapper, IndexCarousel> implements IndexCarouselService {
+
+}
---
+++ src/main/java/com/example/shop_online/service/impl/UserShoppingCartServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.shop_online.service.impl;
+
+import com.example.shop_online.entity.UserShoppingCart;
+import com.example.shop_online.mapper.UserShoppingCartMapper;
+import com.example.shop_online.service.UserShoppingCartService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author whb
+ * @since 2023-11-09
+ */
+@Service
+public class UserShoppingCartServiceImpl extends ServiceImpl<UserShoppingCartMapper, UserShoppingCart> implements UserShoppingCartService {
+
+}
