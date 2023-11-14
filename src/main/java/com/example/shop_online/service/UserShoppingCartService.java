22097ec7ae7a8a788bf0f360206a6945b8a556ec
hbin <11323063@qq.com>
2023-11-14 09:00:23 +0800

    获取购物车列表接口实现

--- src/main/java/com/example/shop_online/service/UserShoppingCartService.java
+++ src/main/java/com/example/shop_online/service/UserShoppingCartService.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.shop_online.query.CartQuery;
 import com.example.shop_online.vo.CartGoodsVO;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -15,4 +17,6 @@ import com.example.shop_online.vo.CartGoodsVO;
  */
 public interface UserShoppingCartService extends IService<UserShoppingCart> {
     CartGoodsVO addShopCart(CartQuery query);
+
+    List<CartGoodsVO> shopCartList(Integer userId);
 }
