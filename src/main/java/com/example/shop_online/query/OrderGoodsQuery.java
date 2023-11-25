16905f7826194443138386b505ca2f542067e913
hbin <11323063@qq.com>
2023-11-25 10:17:12 +0800

    用户菜单管理

---
+++ src/main/java/com/example/shop_online/query/OrderGoodsQuery.java
@@ -0,0 +1,18 @@
+package com.example.shop_online.query;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * author：H
+ * Date：2023/11/25 10:09
+ */
+@Data
+public class OrderGoodsQuery {
+    @Schema(description = "商品id")
+    private Integer id;
+    @Schema(description = "商品数量")
+    private Integer count;
+    @Schema(description = "选中的商品属性")
+    private String skus;
+}
